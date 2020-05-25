46a2a13647084127560681654bbd27e307ea82f0
maxmcfadden88 <maxmcfadden88@gmail.com>
2020-05-25 01:00:34 -0500

    added imagelist + uploadform componz +routes

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="ui secondary pointing menu">
-    <a href="#" class="active item">
+    <router-link to="/" class="active item">
       Image Storage
-    </a>
+    </router-link>
     <div class="right menu">
       <div v-if="isLoggedIn" class="horizontal">
-        <a class="item">Galleries</a>
-        <a class="item">Upload</a>
-        <a class="item">Logout</a>
+        <router-link to="/" class="item">Galleries</router-link>
+        <router-link to="/upload" class="item">Upload</router-link>
+        <a class="item" @click="logout">Logout</a>
       </div>
       <a v-else href="#" class="ui item" @click="login">Login</a>
     </div>
@@ -20,7 +20,7 @@ import { mapActions, mapGetters } from "vuex";
 export default {
   name: "AppHeader",
   computed: mapGetters(["isLoggedIn"]),
-  methods: mapActions(["login"]),
+  methods: mapActions(["login", "logout"]),
 };
 </script>
 
