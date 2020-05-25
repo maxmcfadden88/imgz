9e980c2a56b40c711f9ce180d803a81cf7dea462
maxmcfadden88 <maxmcfadden88@gmail.com>
2020-05-25 15:20:26 -0500

    added fetchImages

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
-    Image List
+    <img v-for="image in allImages" :key="image.id" :src="image.link" />
   </div>
 </template>
 
 <script>
+import { mapActions, mapGetters } from "vuex";
+
 export default {
   name: "ImageList",
+  computed: mapGetters(["allImages"]),
+  methods: mapActions(["fetchImages"]),
+  created() {
+    this.fetchImages();
+  },
 };
 </script>
