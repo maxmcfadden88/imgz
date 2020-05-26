4fedd3e9e25599bd5eb92efcede7b510ad08231b
maxmcfadden88 <maxmcfadden88@gmail.com>
2020-05-25 20:20:49 -0500

    v1 complete

--- src/store/modules/images.js
+++ src/store/modules/images.js
@@ -1,4 +1,5 @@
 import api from "../../api/imgur";
+import { router } from "../../main";
 
 const state = {
   images: [],
@@ -13,6 +14,16 @@ const actions = {
     const { token } = rootState.auth;
     const response = await api.fetchImages(token);
     commit("setImages", response.data.data);
+  },
+  //   async uploadImages(x, images) {
+  //     console.log(images);
+  async uploadImages({ rootState }, images) {
+    // Get access token
+    const { token } = rootState.auth;
+    // Call API module to do the upload
+    await api.uploadImages(images, token);
+    // Redirect our user to ImageList component
+    router.push("/");
   },
 };
 
